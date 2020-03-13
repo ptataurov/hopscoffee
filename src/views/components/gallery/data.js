6d46a6a19002b94931639efa013a1158d8c37a85
Pavel <p.tataurov92@gmail.com>
2020-03-13 17:35:13 +0300

    init commit

---
+++ src/views/components/gallery/data.js
@@ -0,0 +1,10 @@
+module.exports = {
+  top: [
+    require('views/components/gallery/img/1.png'),
+    require('views/components/gallery/img/2.png')
+  ],
+  bottom: [
+    require('views/components/gallery/img/3.png'),
+    require('views/components/gallery/img/4.png')
+  ]
+}
